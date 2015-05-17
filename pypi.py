9d5a8887ba0cc50fe9d0096867e10cc92d7df205
raphaelphys <razibobaid@gmail.com>
2015-05-17 16:56:24 -0400

    Calculation of Pi using Monte Carlo Method
    This code is written as part of homework problem for the (PG)^2 group we
    created at UConn. It consists of two definitions:
    1. circle - which checks whether a point (x,y), derived from random
    number generator using uniform distribution, is within a quadrant of a
    circle.
    2. main - this calculates the value of Pi for a predefined iterations.
    This code is not at all optimized yet! So, use with caution.

---
+++ pypi.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 21:13:56 2015
+
+@author: Razib
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Creating a function for the circle
+#The idea is to take a point from a 
+#psedorandom number generator and check
+#whether it falls within the circle
+def circle(x,y):
+    if (x**2 + y**2) <=1:
+        bullseye = 1
+    else: bullseye = 0
+    return bullseye
+    
+def main(nIter=10**9):
+    bEye=0
+    for n in range(nIter):
+        x = np.random.uniform(0,1)
+        y = np.random.uniform(0,1)
+        bEye +=circle(x,y)
+    PPi=4*(bEye/nIter)
+    #print(PPi)
+    return PPi
+
+piBucket=[]
+for x in range(1000):
+    piBucket.append(main())
+    
+plt.hist(piBucket)
+plt.show()
